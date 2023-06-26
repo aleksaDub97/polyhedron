32077b7daa61157cba6bf432590441c8d24e8b7d
aleksaDub97 <119128130+aleksaDub97@users.noreply.github.com>
2023-06-26 14:48:37 +0300

    Add files via upload

--- tests/test_polyedr.py
+++ tests/test_polyedr.py
@@ -7,18 +7,18 @@ class TestPolyedr:
 
     def test_polyedr_1(self):
         f = Polyedr('data/test_1.geom')
-        return f.get_edge_sum() == 2
+        assert f.get_edge_sum() == 2
 
     def test_polyedr_2(self):
         f = Polyedr('data/test_2.geom')
-        return f.get_edge_sum() == 2
+        assert f.get_edge_sum() == 2
 
     def test_polyedr_3(self):
         f = Polyedr('data/test_3.geom')
-        return f.get_edge_sum() == 0
+        assert f.get_edge_sum() == 0
 
     def test_polyedr_4(self):
         f = Polyedr('data/test_4.geom')
-        return f.get_edge_sum() == 2
+        assert f.get_edge_sum() == 2
 
     # python3 -B -m pytest -p no:cacheprovider tests/test_polyedr.py
