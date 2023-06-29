34ca604c848364a3128e0fc158a8648711f88a83
aleksaDub97 <119128130+aleksaDub97@users.noreply.github.com>
2023-06-29 10:32:09 +0300

    Add files via upload

--- run_shadow.py
+++ run_shadow.py
@@ -7,13 +7,16 @@ from shadow.polyedr import Polyedr
 
 tk = TkDrawer()
 try:
-    for name in ['test_4', "ccc", "cube", "box", "king", "cow"]:
+    for name in ['test_4', "ccc", "cube", "box"]:
         print("=============================================================")
         print(f"Начало работы с полиэдром '{name}'")
         start_time = time()
-        Polyedr(f"data/{name}.geom").draw(tk)
+        Polyedr(f"polyhedron_91/data/{name}.geom").draw(tk)
         delta_time = time() - start_time
+        p = Polyedr(f"polyhedron_91/data/{name}.geom")
         print(f"Изображение полиэдра '{name}' заняло {delta_time} сек.")
+        print("сумма длин проекций ребер, середины которых являются хорошими точками ->", \
+              p.get_edge_sum())
         input("Hit 'Return' to continue -> ")
 except (EOFError, KeyboardInterrupt):
     print("\nStop")
